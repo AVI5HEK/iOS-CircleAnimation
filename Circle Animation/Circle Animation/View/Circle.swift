801045d5caeef4c01eebe989e8bb96ab18185439
Avishek Khan <Avishek@Framgia-2.local>
2017-04-05 09:38:29 +0600

    Project creation

---
+++ Circle Animation/Circle Animation/View/Circle.swift
@@ -0,0 +1,81 @@
+//
+//  Circle.swift
+//  Circle Animation
+//
+//  Created by Avishek Khan on 4/4/17.
+//  Copyright © 2017 framgia. All rights reserved.
+//
+
+import UIKit
+
+class Circle {
+    let rectShape = CAShapeLayer()
+    var bounds = CGRect()
+    var viewController: UIViewController
+    
+    init(viewController: UIViewController) {
+        self.viewController = viewController
+    }
+    
+    func create(bounds: CGRect) -> Circle {
+        self.bounds = bounds
+        rectShape.bounds = bounds
+        rectShape.position = viewController.view.center
+        rectShape.cornerRadius = bounds.width / 2
+        viewController.view.layer.addSublayer(rectShape)
+        
+        return self
+    }
+    
+    func destroy() {
+        rectShape.removeFromSuperlayer()
+    }
+    
+    func color(color color: CGColor) -> Circle {
+        rectShape.fillColor = color
+        return self
+    }
+    
+    func animate(endShape endRect: CGRect, duration: CFTimeInterval) {
+        let radius = CGFloat(sqrt(pow(Float(viewController.view.frame.width), 2) + pow(Float(viewController.view.frame.height), 2)))
+        var animations = [CABasicAnimation]()
+        animations.append(getPathAnimation(bounds: bounds , endRect: CGRect(x: -radius/2, y: -radius/2, width: radius, height: radius), duration: duration))
+        
+        animations.append(getOpacityAnimation(duration: duration))
+        
+        animate(animations: animations)
+    }
+    
+    func animate(animations animations: [CABasicAnimation]) {
+        for animation in animations {
+            rectShape.addAnimation(animation, forKey: animation.keyPath)
+        }
+    }
+    
+    func getPathAnimation(bounds bounds: CGRect, endRect: CGRect, duration: CFTimeInterval) -> CABasicAnimation {
+        
+        let startShape = UIBezierPath(roundedRect: bounds, cornerRadius: 50).CGPath
+        let endShape = UIBezierPath(roundedRect: endRect, cornerRadius: 500).CGPath
+        
+        rectShape.path = startShape
+        
+        let animation = CABasicAnimation(keyPath: "path")
+        animation.toValue = endShape
+        animation.duration = duration
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+        animation.fillMode = kCAFillModeBoth
+        animation.removedOnCompletion = false
+        
+        return animation
+    }
+    
+    func getOpacityAnimation(duration duration: CFTimeInterval) -> CABasicAnimation {
+        let animationOpacity = CABasicAnimation(keyPath: "opacity")
+        animationOpacity.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)
+        animationOpacity.duration = duration
+        animationOpacity.fromValue = 0
+        animationOpacity.toValue = 1
+        
+        return animationOpacity
+    }
+}
---
+++ Circle Animation/Circle Animation/ViewController/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  Circle Animation
+//
+//  Created by Avishek Khan on 4/3/17.
+//  Copyright © 2017 framgia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelOceanize: UILabel!
+    
+    let notificationCenter = NSNotificationCenter.defaultCenter()
+    let rectShape = CAShapeLayer()
+    var bounds = CGRect()
+    var circle: Circle? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addObservers()
+    }
+    
+    func addObservers() {
+        notificationCenter.addObserver(self,
+                                       selector:#selector(ViewController.setUpUI),
+                                       name:UIApplicationDidBecomeActiveNotification,
+                                       object:nil)
+        notificationCenter.addObserver(self,
+                                       selector:#selector(ViewController.clearUI),
+                                       name:UIApplicationWillResignActiveNotification,
+                                       object:nil)
+    }
+    
+    func setUpUI() {
+        labelOceanize.hidden = true
+        
+        let bounds = CGRect(x: 0, y: 0, width: 0, height: 0)
+        let radius = CGFloat(sqrt(pow(Float(self.view.frame.width), 2) + pow(Float(self.view.frame.height), 2)))
+        let duration = CFTimeInterval(1)
+        
+        var animations = [CABasicAnimation]()
+        
+        circle = Circle(viewController: self)
+        
+        if let _circle = circle {
+            animations.append(_circle.getPathAnimation(bounds: bounds , endRect: CGRect(x: -radius/2, y: -radius/2, width: radius, height: radius), duration: duration))
+            
+            animations.append(_circle.getOpacityAnimation(duration: duration))
+            
+            animations[0].delegate = self
+            
+            //color code #01C9F1
+            _circle.create(bounds).color(color: UIColor(rgb: 0x01C9F1).CGColor).animate(animations: animations)
+        }
+    }
+    
+    func clearUI() {
+        if let _circle = circle {
+            _circle.destroy()
+        }
+    }
+    
+    deinit {
+        notificationCenter.removeObserver(self)
+    }
+    
+    override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        showLabel()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func showLabel() {
+        labelOceanize.hidden = false
+        self.view.layer.addSublayer(labelOceanize.layer)
+        
+        if let _circle = circle {
+            let animationOpacity = _circle.getOpacityAnimation(duration: 1)
+            labelOceanize.layer.addAnimation(animationOpacity, forKey: animationOpacity.keyPath)
+        }
+    }
+}
